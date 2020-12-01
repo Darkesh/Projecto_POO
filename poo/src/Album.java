9784e77b8bd219b73b90fc5a723aeb66299259b5
joaoeespada <joaodany@gmail.com>
2020-12-01 17:56:15 +0000

    album e imagemcometiqueta

--- poo/src/Album.java
+++ poo/src/Album.java
@@ -1,2 +1,39 @@
 public class Album {
+
+    private void adicionarImg() {
+
+    }
+
+    private void imgExibicao() {
+
+    }
+
+    private void imgMetadeAnt() {
+
+    }
+
+    private void imgMetadeSeg() {
+
+    }
+
+    private void mudarImgSeg() {
+
+    }
+
+    private void mudarImgAnt() {
+
+    }
+
+    private void ampliarImg() {
+
+    }
+
+    private void blurImg() {
+
+    }
+
+    private void mostrarAlbum() {
+
+    }
+
 }
--- poo/src/ImagemComEtiqueta.java
+++ poo/src/ImagemComEtiqueta.java
@@ -1,2 +1,23 @@
 public class ImagemComEtiqueta {
+
+    private String nome;
+    private float largura;
+    private float altura;
+
+    private void obterNome() {
+
+    }
+
+    private void mudarNome() {
+
+    }
+
+    private void obterLargura() {
+
+    }
+
+    private void obterAltura() {
+
+    }
+
 }
