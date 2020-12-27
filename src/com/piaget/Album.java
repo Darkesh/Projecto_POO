8f0963565fb450223be288f7e76a304513e25aa6
joaoeespada <joaodany@gmail.com>
2020-12-27 13:55:06 +0000

    sadqw

--- src/com/piaget/Album.java
+++ src/com/piaget/Album.java
@@ -1,12 +1,22 @@
 package com.piaget;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Album {
-    List<ImagemComEtiqueta> asMinhasImagens;
+public class Album<imagem1> {
+    List<ImagemComEtiqueta> asMinhasImagens = new ArrayList<ImagemComEtiqueta>();
     ManipuladorDeImagens manipuladorDeImagens;
 
-    ImagemComEtiqueta imagem1 = new ImagemComEtiqueta("donald_trump.jpg");
+    ImagemComEtiqueta imagem1 = new ImagemComEtiqueta("C:\\Users\\Joao Espada\\Documents\\GitHub\\Projecto_POO\\imagens\\jerry.png");
 
     ImagemComEtiqueta imagem1reduzida = (ImagemComEtiqueta) ManipuladorDeImagens.reduzImagem(imagem1, 2);
+
+    public void addImage (ImagemComEtiqueta imagem1) {
+        asMinhasImagens.add(imagem1);
+    }
+
+    public void dataImages (){
+        String nome = imagem1.nomeImage;
+        System.out.println(nome);
+    }
 }
